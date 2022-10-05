0b50d304a9c7a9b65b8f43e5cc633f6a4ec00b0a
Amr1t-Ar0ra <109554417+Amr1t-Ar0ra@users.noreply.github.com>
2022-10-05 16:17:45 -0400

    Delete read_Data.m

--- read_Data.m
+++
@@ -1,5 +0,0 @@
-function [output] = read_Data(filename)
-    filepath = "C:\Data\Purdue\Purdue Space Program\High Altitude\Mass Estimation\PSP-HA-Structures-main\" + filename + ".txt";
-    fid = fopen(filepath, 'r');
-    output = fscanf(fid, '%f');
-end
